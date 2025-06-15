336f5720e7375d119f268647b66f96f3e5411515
Reid-T-W <rediettadesse100@gmail.com>
2025-06-15 17:55:38 +0300

    fix: original dataset is converted to csv instead of txt

--- scripts/convert-data.py
+++ scripts/convert-data.py
@@ -4,7 +4,7 @@ def convert_txt_to_csv():
     # Load data from txt
     print("Starting data conversion")
     df = pd.read_csv("../data/raw/MachineLearningRating_v3.txt", delimiter="|")
-    df.to_csv("../data/raw/MachineLearningRating_v3_csv.txt", index=False)
+    df.to_csv("../data/raw/MachineLearningRating_v3_csv.csv", index=False)
     print("Conversation from txt to csv successful")
         
 if __name__ == "__main__":
